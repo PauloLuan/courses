fcc4b61021ffc900edae5219e692b78d298c038b
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-23 13:07:53 -0300

    added some scenarios

--- pluralsight/node/nodejs_testing_strategies/lib/subscriptions/test/mission_application_spec.js
+++ pluralsight/node/nodejs_testing_strategies/lib/subscriptions/test/mission_application_spec.js
@@ -1,7 +1,14 @@
 var assert = require('assert');
 
 describe('Applying for a mission', function() {
-  describe('using a valid email, last, first', function() {
+  describe('using a valid name, first, last, email, age, height, weight', function() {
     it('let´s them in');
+    it('reports a valid name');
+    it('reports a valid first');
+    it('reports a valid last');
+    it('reports a valid email');
+    it('reports a valid age');
+    it('reports a valid height');
+    it('reports a valid weight');
   });
 });
