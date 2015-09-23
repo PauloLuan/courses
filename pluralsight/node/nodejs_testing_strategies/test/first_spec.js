449fb4f264f82ca74cd105626dd63b542205593e
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-23 10:34:16 -0300

    added initial structure

---
+++ pluralsight/node/nodejs_testing_strategies/test/first_spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('A Feature', function() {
+  describe('A Scenario', function() {
+    it('Does something expected', function() {
+      assert('true');
+    });
+  });
+});
