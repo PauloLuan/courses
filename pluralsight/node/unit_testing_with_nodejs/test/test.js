d440141a3b3db4bfa1f1661f77a4efd2c38a3caf
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-23 09:57:58 -0300

    added skip and xit to test

--- pluralsight/node/unit_testing_with_nodejs/test/test.js
+++ pluralsight/node/unit_testing_with_nodejs/test/test.js
@@ -11,5 +11,13 @@ describe('init describe', function() {
     expect(1).to.not.be.true;
   });
 
+  xit('should not execute this function because we have an "x" before the it', function() {
+    // it works as well either with describe
+    expect(true).to.be.true;
+  })
+
+  it.skip('other way to do the same as "x" is to put .skip after the it', function() {
+    expect(true).to.be.true;
+  });
 
 });
