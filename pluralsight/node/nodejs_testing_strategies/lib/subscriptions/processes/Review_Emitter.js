ad9cf2e0bf85e5005a1702cf229156bf2c820b77
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-26 19:31:00 -0300

    added event emitter api

--- pluralsight/node/nodejs_testing_strategies/lib/subscriptions/processes/Review_Emitter.js
+++ pluralsight/node/nodejs_testing_strategies/lib/subscriptions/processes/Review_Emitter.js
@@ -1,9 +1,25 @@
-var Emitter = require('events').EventEmitter;
-var util = require('util');
+var Emitter = require('events').EventEmitter,
+  util = require('util'),
+  self;
 
 var ReviewProcess = function(args) {
+  self = this;
 
-}
+  // event path
+  self.on("application-received", self.ensureAppIsValid);
+  self.on("validated", self.findNextMission);
+  self.on("mission-selected", self.roleIsAvailable);
+  self.on("role-available", self.ensureRoleCompatible);
+  self.on("role-compatible", self.acceptApplication);
+  self.on("invalid", self.denyApplication);
+};
+
+ReviewProcess.prototype.ensureAppIsValid = function() {};
+ReviewProcess.prototype.findNextMission = function() {};
+ReviewProcess.prototype.roleIsAvailable = function() {};
+ReviewProcess.prototype.ensureRoleCompatible = function() {};
+ReviewProcess.prototype.acceptApplication = function() {};
+ReviewProcess.prototype.denyApplication = function() {};
 
 util.inherits();
 module.exports = ReviewProcess;
