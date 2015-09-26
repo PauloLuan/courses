be21da3dad5de55f26bacb4b48bb7861b3ebf7ef
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-26 19:31:00 -0300

    implemented first version of review application processes using event emmiter

--- pluralsight/node/nodejs_testing_strategies/lib/subscriptions/processes/Review_Emitter.js
+++ pluralsight/node/nodejs_testing_strategies/lib/subscriptions/processes/Review_Emitter.js
@@ -1,6 +1,7 @@
 var Emitter = require('events').EventEmitter,
   util = require('util'),
-  self;
+  self,
+  callback;
 
 var ReviewProcess = function(args) {
   self = this;
@@ -14,12 +15,51 @@ var ReviewProcess = function(args) {
   self.on("invalid", self.denyApplication);
 };
 
-ReviewProcess.prototype.ensureAppIsValid = function() {};
-ReviewProcess.prototype.findNextMission = function() {};
-ReviewProcess.prototype.roleIsAvailable = function() {};
-ReviewProcess.prototype.ensureRoleCompatible = function() {};
-ReviewProcess.prototype.acceptApplication = function() {};
-ReviewProcess.prototype.denyApplication = function() {};
+ReviewProcess.prototype.ensureAppIsValid = function() {
+  if (app.isValid()) {
+    self.emmit('validated', app);
+  } else {
+    self.emmit('invalid', app.validationMessage());
+  }
+};
+
+ReviewProcess.prototype.findNextMission = function(app) {
+  app.mission = {
+    commander: null,
+    pilot: null,
+    MAVPilot: null,
+    passengers: []
+  };
+
+  self.emmit('mission-selected', app);
+};
+
+ReviewProcess.prototype.roleIsAvailable = function(app) {
+  self.emmit('role-available', app);
+};
+
+ReviewProcess.prototype.ensureRoleCompatible = function(app) {
+  self.emmit('role-compatible', app);
+};
+
+ReviewProcess.prototype.acceptApplication = function(app) {
+  callback(null, {
+    success: true,
+    message: 'Welcome to the Mars Program!'
+  });
+};
+
+ReviewProcess.prototype.denyApplication = function(message) {
+  callback(null, {
+    success: false,
+    message: message
+  });
+};
+
+ReviewProcess.prototype.processApplication = function(app, next) {
+  self.emmit('application-received');
+  callback = next;
+};
 
 util.inherits();
 module.exports = ReviewProcess;
