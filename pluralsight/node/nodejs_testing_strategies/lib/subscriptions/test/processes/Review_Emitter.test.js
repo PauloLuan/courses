2f226b92fd74e7c9e932bd2a800ea33a5a0ef254
Paulo Luan <pauloluan.inova@gmail.com>
2015-09-29 19:31:00 -0300

    added working tests

--- pluralsight/node/nodejs_testing_strategies/lib/subscriptions/test/processes/Review_Emitter.test.js
+++ pluralsight/node/nodejs_testing_strategies/lib/subscriptions/test/processes/Review_Emitter.test.js
@@ -1,32 +1,30 @@
 var assert = require('assert'),
-  ReviewProcess = require('../../processes/Review_Emitter.js'),
-  MembershipApplication = require('../../models/membership_application.js');
+  ReviewProcess = require('../../processes/Review_Emitter'),
+  MembershipApplication = require('../../models/membership_application');
 
 describe('The Review Process', function() {
-  it('Receiving a valid  application', function() {
-    var validApp, decision;
+  var validApp, decision;
 
-    before(function(done) {
-      validApp = new MembershipApplication({
-        first: 'John',
-        last: 'Doe',
-        email: 'johndoe@test.com',
-        age: 33,
-        height: 70,
-        weight: 200
-      });
-
-      var review = new ReviewProcess();
-
-      review.processApplication(validApp, function(err, result) {
-        decision = result;
-        done();
-      });
+  before(function(done) {
+    validApp = new MembershipApplication({
+      first: 'John',
+      last: 'Doe',
+      email: 'johndoe@test.com',
+      age: 33,
+      height: 70,
+      weight: 200
     });
 
-    it('returns success', function() {
-      assert(decision.success, decision.message);
-    });
+    var review = new ReviewProcess();
 
+    review.processApplication(validApp, function(err, result) {
+      decision = result;
+      done();
+    });
   });
+
+  it('returns success', function() {
+    assert(decision.success, decision.message);
+  });
+
 });
